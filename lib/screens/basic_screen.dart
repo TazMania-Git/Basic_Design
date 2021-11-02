741bf0a26dae91cc52daa7f8b79c938d294b69b3
TazMania <nallinon@gmail.com>
2021-11-01 23:45:44 -0300

    Finish first screen

--- lib/screens/basic_screen.dart
+++ lib/screens/basic_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BasicScreen extends StatelessWidget {
@@ -10,18 +11,98 @@ class BasicScreen extends StatelessWidget {
         child: Column(
           children: [
             Image(image: AssetImage('assets/landscapemexico.jpg')),
-            Row(
-              children: [
-                Text("Playa del Carmen"),
-                Icon(
-                  Icons.star,
-                  color: Colors.red,
-                ),
-                Text("41"),
-              ],
+            Title(),
+            ButtonSelection(),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Text(
+                  "Non commodo ad consectetur nisi incididunt do irure exercitation sint elit cupidatat. Nulla culpa aute excepteur dolor labore. Cupidatat reprehenderit pariatur occaecat id. Excepteur nulla commodo laboris ea dolore laboris quis sunt enim consectetur ex ex excepteur. Ullamco adipisicing dolor non officia nisi reprehenderit ullamco tempor incididunt."),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ButtonSelection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(
+            icon: Icons.call,
+            text: "CALL",
+          ),
+          CustomButton(
+            icon: Icons.near_me,
+            text: "ROUTE",
+          ),
+          CustomButton(
+            icon: Icons.share,
+            text: "SHARE",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({Key? key, required this.icon, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 40,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: Colors.blue, fontSize: 15),
+        )
+      ],
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Playa del Carmen",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Hotel Azul Fives",
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text("41"),
+        ],
       ),
     );
   }
